f5350a1e33da41e280bf5bfcbba3329ce0c6a5ea
Jari Sokka <sokkajar@gmail.com>
2021-05-31 13:26:09 +0300

    print courses ready

---
+++ src/commands/Student/printCourses.js
@@ -0,0 +1,23 @@
+const { getRoleFromCategory, context } = require("../../util");
+
+
+const execute = async (message) => {
+  const { guild } = context;
+  const rows = guild.channels.cache
+    .filter((ch) => ch.type === "category" && ch.name.startsWith("📚"))
+    .map((ch) => {
+      const courseFullName = ch.name.replace("📚", "").trim();
+      const courseRole = getRoleFromCategory(ch.name);
+      return `${courseFullName} - \`!join ${courseRole}\``;
+    })
+    .sort((a, b) => a.localeCompare(b));
+
+  message.reply("\n" + rows.join("\n"));
+};
+
+module.exports = {
+  name: "courses",
+  args: false,
+  description: "Prints out the courses to use with \`!join\` and \`!leave\`. NOT IMPLEMENTED YET!",
+  execute,
+};
