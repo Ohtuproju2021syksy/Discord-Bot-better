0e59b011f9204e3c7b714e88a9cca671b2f9034c
Elias Lehtinen <elias.lehtinen01@gmail.com>
2021-06-06 13:34:55 +0300

    Refactor create course command, fix bug in remove command

--- src/commands/faculty/create.js
+++ src/commands/faculty/create.js
@@ -7,134 +7,101 @@ const createCategoryName = (courseString) => `📚 ${courseString}`;
  * @param {Object} channelObject
  * @param {Discord.GuildChannel} parent
  */
-const findOrCreateChannel = (channelObject, guild) => {
-  const { name, options } = channelObject;
-  const alreadyExists = guild.channels.cache.find(
-    (c) => c.type === options.type && c.name === name,
-  );
-  if (alreadyExists) return alreadyExists;
-  return guild.channels.create(name, options);
+const findOrCreateChannel = async (channelObject, guild) => {
+  const { name, parent, options } = channelObject;
+  return guild.channels.cache.find(
+    (c) => c.type === options.type && c.name === name && c.parent === parent
+  ) || await guild.channels.create(name, options);
 };
 
-/**
- *
- * @param {String} courseName
- * @param {String} roleName
- * @param {Discord.Role} studentRole
- * @param {Discord.Role} adminRole
- */
-const findOrCreateCategoryWithName = async (
-  courseName,
-  roleName,
-  studentRole,
-  adminRole,
-  guild,
-) => {
-  const categoryName = createCategoryName(courseName, roleName);
-  const permissionOverwrites = [
-    {
-      id: guild.id,
-      deny: ["VIEW_CHANNEL"],
-    },
-    {
-      id: guild.me.roles.highest,
-      allow: ["VIEW_CHANNEL"],
-    },
-    {
-      id: adminRole.id,
-      allow: ["VIEW_CHANNEL"],
-    },
-    {
-      id: studentRole.id,
-      allow: ["VIEW_CHANNEL"],
-    },
-  ];
+const getPermissionOverwrites = (guild, admin, student) => ([
+  {
+    id: guild.id,
+    deny: ["VIEW_CHANNEL"],
+  },
+  {
+    id: guild.me.roles.highest,
+    allow: ["VIEW_CHANNEL"],
+  },
+  {
+    id: admin.id,
+    allow: ["VIEW_CHANNEL"],
+  },
+  {
+    id: student.id,
+    allow: ["VIEW_CHANNEL"],
+  },
+]);
 
-  const categoryObject = {
-    name: categoryName,
+const getChannelObjects = (guild, admin, student, roleName, category) => [
+  {
+    name: `${roleName}_announcement`,
     options: {
-      type: "category",
-      permissionOverwrites,
-    },
-  };
-
-  return findOrCreateChannel(categoryObject, guild);
-};
-
-/**
- *
- * @param {Discord.GuildMember} user
- * @param {String} courseName
- */
-const createCourse = async (user, args, guild) => {
-  if (user.roles.highest.name !== "teacher") { throw new Error("You have no power here!"); }
-
-  const roleName = getRoleFromCategory(args);
-
-  const studentRole = await findOrCreateRoleWithName(roleName, guild);
-  const adminRole = await findOrCreateRoleWithName(`${roleName} admin`, guild);
-
-  const category = await findOrCreateCategoryWithName(
-    args,
-    roleName,
-    studentRole,
-    adminRole,
-    guild,
-  );
-
-  const CHANNELS = [
-    {
-      name: `${roleName}_announcement`,
-      options: {
-        type: "text",
-        description: "Messages from course admins",
-        parent: category,
-        permissionOverwrites: [
-          {
-            id: guild.id,
-            deny: ["VIEW_CHANNEL"],
-          },
-          {
-            id: studentRole,
-            deny: ["SEND_MESSAGES"],
-            allow: ["VIEW_CHANNEL"],
-          },
-          {
-            id: adminRole,
-            allow: ["VIEW_CHANNEL", "SEND_MESSAGES"],
-          },
-        ],
-      },
-    },
-    {
-      name: `${roleName}_general`,
+      type: "text",
+      description: "Messages from course admins",
       parent: category,
-      options: { type: "text", parent: category, permissionOverwrites: [] },
+      permissionOverwrites: [
+        {
+          id: guild.id,
+          deny: ["VIEW_CHANNEL"],
+        },
+        {
+          id: student,
+          deny: ["SEND_MESSAGES"],
+          allow: ["VIEW_CHANNEL"],
+        },
+        {
+          id: admin,
+          allow: ["VIEW_CHANNEL", "SEND_MESSAGES"],
+        },
+      ],
     },
-    {
-      name: `${roleName}_questions`,
-      parent: category,
-      options: { type: "text", parent: category, permissionOverwrites: [] },
-    },
-    {
-      name: `${roleName}_voice`,
-      parent: category,
-      options: { type: "voice", parent: category, permissionOverwrites: [] },
-    },
-  ];
+  },
+  {
+    name: `${roleName}_general`,
+    parent: category,
+    options: { type: "text", parent: category, permissionOverwrites: [] },
+  },
+  {
+    name: `${roleName}_questions`,
+    parent: category,
+    options: { type: "text", parent: category, permissionOverwrites: [] },
+  },
+  {
+    name: `${roleName}_voice`,
+    parent: category,
+    options: { type: "voice", parent: category, permissionOverwrites: [] },
+  },
+];
 
-  await CHANNELS.reduce(async (promise, channel) => {
-    await promise;
-
-    return findOrCreateChannel(channel, guild);
-  }, Promise.resolve());
-
-};
-
+const getCategoryObject = (categoryName, permissionOverwrites) => ({
+  name: categoryName,
+  options: {
+    type: "category",
+    permissionOverwrites,
+  },
+});
 
 const execute = async (message, args) => {
-  const who = message.member;
-  await createCourse(who, args, message.guild);
+  const courseName = args;
+  const member = message.member;
+  const roleName = getRoleFromCategory(courseName);
+  const guild = message.guild;
+
+  // Roles
+  const student = await findOrCreateRoleWithName(roleName, guild);
+  const admin = await findOrCreateRoleWithName(`${roleName} admin`, guild);
+
+  // Category
+  const categoryName = createCategoryName(courseName, roleName);
+  const categoryObject = getCategoryObject(categoryName, getPermissionOverwrites(guild, admin, student));
+  const category = await findOrCreateChannel(categoryObject, guild);
+
+  // Channels
+  const channelObjects = getChannelObjects(guild, admin, student, roleName, category);
+  await Promise.all(channelObjects.map(
+    async channelObject => await findOrCreateChannel(channelObject, guild)
+  ));
 };
 
 module.exports = {
