d6d6cd46010ef5a5944d3fbe5af78a8f006c14ac
larikkai <kai.larikka@kolumbus.fi>
2021-06-09 10:06:50 +0300

    Add more jest tests
    - guide permissions
    - guide has pinned message

--- __tests__/initialize.test.js
+++ __tests__/initialize.test.js
@@ -14,4 +14,28 @@ describe("Initialize", () => {
     const foundChannel = guild.channels.cache.find(c => c.type === "text" && c.name === channelName);
     expect(foundChannel.name).toBe(channelName);
   });
+
+  test("bot has permission to send messages on guide channel", async () => {
+    const channelName = "guide";
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const foundChannel = guild.channels.cache.find(c => c.type === "text" && c.name === channelName);
+    const permission = foundChannel.permissionsFor(process.env.BOT_TEST_ID).has(["SEND_MESSAGES"]);
+    expect(permission).toBe(true);
+  });
+
+  test("guild member has not permission to send messages on guide channel", async () => {
+    const channelName = "guide";
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const foundChannel = guild.channels.cache.find(c => c.type === "text" && c.name === channelName);
+    const permission = foundChannel.permissionsFor(guild.id).has(["SEND_MESSAGES"]);
+    expect(permission).toBe(false);
+  });
+
+  test("guide channel has pinned message", async () => {
+    const channelName = "guide";
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const foundChannel = guild.channels.cache.find(c => c.type === "text" && c.name === channelName);
+    const pinned = await foundChannel.messages.fetchPinned().then(arr => arr.first().pinned);
+    expect(pinned).toBe(true);
+  });
 });
