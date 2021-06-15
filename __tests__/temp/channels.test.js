1967f442f9894be731a033aac499acb86cb14a03
larikkai <kai.larikka@kolumbus.fi>
2021-06-15 12:40:52 +0300

    Move tests to temp

---
+++ __tests__/temp/channels.test.js
@@ -0,0 +1,33 @@
+// jest.mock("../src/service.js");
+// jest.mock("../src/index.js");
+
+const { client } = require("../src/index.js");
+
+const { createChannelInCategory, testi } = require("../src/service.js");
+
+describe("channels", () => {
+  /* const client = ({
+    guild: {
+      fetch: jest.fn(),
+      channels: {
+        create: jest.fn(),
+      },
+    },
+  }); */
+  test.only("channel is created in category", async () => {
+    testi();
+    const channelName = "testikanava";
+    const categoryName = "testikategoria";
+    const guild = client.guilds;
+    /* const channel = await createChannelInCategory(guild, channelName, categoryName);
+    const category = guild.channels.cache.find(c => c.type === "category" && c.name === categoryName);
+    const createdChannelName = channel.name;
+    const createCategoryName = category.name;
+
+    expect(createdChannelName).toBe(channelName);
+    expect(createCategoryName).toBe(categoryName);
+
+    await channel.delete();
+    await category.delete();*/
+  });
+});
