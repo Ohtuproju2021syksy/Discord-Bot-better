389bb44226352636102ab977b0d76af17e1cfef8
larikkai <kai.larikka@kolumbus.fi>
2021-06-16 14:29:11 +0300

    Update help test and add more jest mocks

--- __tests__/help.test.js
+++ __tests__/help.test.js
@@ -1,31 +1,33 @@
-const { client } = require("../src/index.js");
-const { teacherMessage, teacherData, studentMessage, studentData,
-  teacherMessageHelpJoin, teacherJoinData, studentMessageHelpIns, studentInsData } = require("./mocks.js");
+jest.mock("../src/service");
 
+const { client } = require("../src/index.js");
 const { execute } = require("../src/events/message");
+
+const { teacherMessageHelp, teacherData, studentMessageHelp, studentData,
+  teacherMessageHelpJoin, teacherJoinData, studentMessageHelpIns, studentInsData } = require("./mocks.js");
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe("channels", () => {
+describe("help command", () => {
   test("help with teacher role should see all commands", async () => {
-    await execute(teacherMessage, client);
-    expect(teacherMessage.channel.send).toHaveBeenCalledTimes(1);
-    expect(teacherMessage.channel.send).toHaveBeenCalledWith(teacherData, { "split": true });
+    await execute(teacherMessageHelp, client);
+    expect(teacherMessageHelp.channel.send).toHaveBeenCalledTimes(1);
+    expect(teacherMessageHelp.channel.send).toHaveBeenCalledWith(teacherData, { "split": true });
   });
 
   test("help with student role cannot see teacher commands", async () => {
-    await execute(studentMessage, client);
-    expect(studentMessage.channel.send).toHaveBeenCalledTimes(1);
-    expect(studentMessage.channel.send).toHaveBeenCalledWith(studentData, { "split": true });
+    await execute(studentMessageHelp, client);
+    expect(studentMessageHelp.channel.send).toHaveBeenCalledTimes(1);
+    expect(studentMessageHelp.channel.send).toHaveBeenCalledWith(studentData, { "split": true });
   });
 
   test("help with invalid arg should give error", async () => {
-    studentMessage.content = "!help notvalidcommand";
-    await execute(studentMessage, client);
-    expect(studentMessage.reply).toHaveBeenCalledTimes(1);
-    expect(studentMessage.reply).toHaveBeenCalledWith("that's not a valid command!");
+    studentMessageHelp.content = "!help notvalidcommand";
+    await execute(studentMessageHelp, client);
+    expect(studentMessageHelp.reply).toHaveBeenCalledTimes(1);
+    expect(studentMessageHelp.reply).toHaveBeenCalledWith("that's not a valid command!");
   });
 
   test("help with valid arg should give correct command info", async () => {
