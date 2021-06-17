1ea683c48f5f3c6d8d4a82f90e8495d496d907bb
larikkai <kai.larikka@kolumbus.fi>
2021-06-17 21:27:31 +0300

    Fix jest channels test

--- __tests__/channels.test.js
+++ __tests__/channels.test.js
@@ -1,13 +1,11 @@
 const { client } = require("../src/index.js");
-
-const { createChannelInCategory, testi } = require("../src/service.js");
+const { createChannelInCategory } = require("../src/service.js");
 
 describe("channels", () => {
-  test.only("channel is created in category", async () => {
-    testi();
+  test("channel is created in category", async () => {
     const channelName = "testikanava";
     const categoryName = "testikategoria";
-    const guild = client.guilds;
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
     const channel = await createChannelInCategory(guild, channelName, categoryName);
     const category = guild.channels.cache.find(c => c.type === "category" && c.name === categoryName);
     const createdChannelName = channel.name;
