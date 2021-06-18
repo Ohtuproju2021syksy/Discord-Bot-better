f30fa1651129460d4284a078421c7985bde937ac
Jari Sokka <sokkajar@gmail.com>
2021-06-18 15:54:17 +0300

    fix leave command remove also admin course role

--- src/commands/student/leave.js
+++ src/commands/student/leave.js
@@ -1,14 +1,11 @@
-const { possibleRolesArray } = require("../../service");
-
 const execute = async (message, args) => {
   const roleString = args;
   const member = message.member;
   const guild = message.guild;
 
-  const roles = possibleRolesArray(guild);
-  const role = roles.find(
-    (r) => r.name === roleString,
-  );
+  const role = guild.roles.cache.filter(
+    (r => (r.name === roleString || r.name === `${roleString} admin`)));
+
   if (!role) throw new Error("Role does not exist or is not available");
   await member.roles.remove(role);
   await member.fetch(true);
