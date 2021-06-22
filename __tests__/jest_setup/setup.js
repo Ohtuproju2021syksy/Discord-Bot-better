19bb805bf89f0396bb99fd578491e11438aa4a0a
larikkai <kai.larikka@kolumbus.fi>
2021-06-22 23:22:26 +0300

    initial postgresql localhost

--- __tests__/jest_setup/setup.js
+++ __tests__/jest_setup/setup.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { client } = require("../../src/index.js");
+const { sequelize } = require("../../src/dbInit");
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 beforeAll(async () => {
@@ -14,6 +15,13 @@ for (const file of testFolder) {
 }
 
 afterAll(async () => {
+  try {
+    await sequelize.close();
+    console.log("Connection closed successfully.");
+  }
+  catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
   client.destroy();
   // avoid jest open handle error
   await new Promise(resolve => setTimeout(() => resolve(), 3000));
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { initializeApplicationContext } = require("../init");
+const { sequelize } = require("../dbInit");
 
 const execute = async (client) => {
   client.guild = await client.guilds.fetch(process.env.GUILD_ID);
@@ -7,6 +8,13 @@ const execute = async (client) => {
   client.guild.inv = await client.guild.fetchInvites();
   initializeApplicationContext(client);
   console.log(`Logged in as ${client.user.tag}!`);
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  }
+  catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
 
 module.exports = {
