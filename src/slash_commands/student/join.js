dbeb0a3ec7bee9a6e07fe6226f0c5319b268eb03
larikkai <kai.larikka@kolumbus.fi>
2021-07-02 02:18:00 +0300

    Uniform slash command join and leave

--- src/slash_commands/student/join.js
+++ src/slash_commands/student/join.js
@@ -1,44 +1,39 @@
-const { possibleRolesArray, getRoleFromCategory } = require("../../service");
+const { getRoleFromCategory } = require("../../service");
 const { sendEphemeral } = require("../utils");
 const { client } = require("../../index");
 
 const getChoices = () => {
-  const guild = client.guilds.cache.get(process.env.GUILD_ID);
-  const courseNames = guild.channels.cache
+  const choices = client.guild.channels.cache
     .filter(({ type, name }) => type === "category" && name.startsWith("📚"))
-    .map(({ name }) => getRoleFromCategory(name));
-
-  const choices = courseNames.map(courseName => ({ name: courseName, value: courseName }));
+    .map(({ name }) => getRoleFromCategory(name))
+    .map(courseName => ({ name: courseName, value: courseName }));
   // console.log(choices);
   return choices;
 };
 
 const execute = async (interaction) => {
   const roleString = interaction.data.options[0].value;
+  const member = client.guild.members.cache.get(interaction.member.user.id);
+  const courseRole = client.guild.roles.cache.find(r => r.name === roleString);
 
-  const guild = client.guild;
-  const member = guild.members.cache.get(interaction.member.user.id);
+  const courseRoles = client.guild.roles.cache
+    .filter(r => (r.name === `${roleString} admin` || r.name === `${roleString}`))
+    .map(r => r.name);
 
-  const roles = possibleRolesArray(guild);
-  const role = roles.find(
-    (r) => r.name === roleString,
-  );
+  if(!courseRoles.length) return sendEphemeral(client, interaction, `Invalid course name: ${roleString}`);
+  if(member.roles.cache.some(r => courseRoles.includes(r.name))) return sendEphemeral(client, interaction, `You are already on a ${roleString} course.`);
 
-  if (!role) throw new Error("Role does not exist or is not available");
+  const teacher = member.roles.cache.find(r => r.name === "teacher");
+  teacher ? client.guild.roles.cache
+    .filter(r => (r.name === `${roleString} admin` || r.name === `${roleString}`))
+    .map(async r => member.roles.add(r)) : await member.roles.add(courseRole);
 
-  if (member.roles.cache.has(role.id)) {
-    sendEphemeral(client, interaction, `You already belong to ${roleString} channel.`);
-  }
-  else {
-    await member.roles.add(role);
-    await member.fetch(true);
-    sendEphemeral(client, interaction, `Joined ${roleString} channel. Enjoy your studies!`);
-  }
+  sendEphemeral(client, interaction, `You have been added to a ${roleString} course.`);
 };
 
 module.exports = {
   name: "join",
-  description: "Join a course.",
+  description: "Join a course, e.g. `!join ohpe`",
   usage: "[course name]",
   args: true,
   joinArgs: true,
@@ -53,5 +48,4 @@ module.exports = {
     },
   ],
   execute,
-  getChoices,
 };
--- src/slash_commands/student/leave.js
+++ src/slash_commands/student/leave.js
@@ -19,25 +19,28 @@ const execute = async (interaction) => {
     .filter(role => (role.name === `${roleString} admin` || role.name === `${roleString}`))
     .map(role => role.name);
 
-  if (!courseRoles.length) return sendEphemeral(client, interaction, `Invalid course name: ${roleString}`);
-  if(!member.roles.cache.some((r) => courseRoles.includes(r.name))) return sendEphemeral(client, interaction, `You are not in ${roleString} channel.`);
+  if(!courseRoles.length) return sendEphemeral(client, interaction, `Invalid course name: ${roleString}`);
+  if(!member.roles.cache.some((r) => courseRoles.includes(r.name))) return sendEphemeral(client, interaction, `You are not on a ${roleString} course.`);
+
   await member.roles.cache
     .filter(role => courseRoles.includes(role.name))
     .map(async role => await member.roles.remove(role));
   await member.fetch(true);
-  sendEphemeral(client, interaction, `Left ${roleString} channel.`);
+
+  sendEphemeral(client, interaction, `You have been removed from the ${roleString} course.`);
 };
 
 module.exports = {
   name: "leave",
   description: "Remove you from the course, e.g. `!leave ohpe`",
+  usage: "[course name]",
   args: true,
   joinArgs: true,
   guide: true,
   options: [
     {
       name: "course",
-      description: "Course to join.",
+      description: "Course to leave.",
       type: 3,
       choices: getChoices(client),
       required: true,
