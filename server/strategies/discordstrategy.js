31a475676c1acdb133df0915010240e3561f00f5
larikkai <kai.larikka@kolumbus.fi>
2021-07-08 23:33:18 +0300

    Add state

--- server/strategies/discordstrategy.js
+++ server/strategies/discordstrategy.js
@@ -3,26 +3,31 @@ const Discord = require("discord.js");
 const passport = require("passport");
 
 passport.use(new DiscordStrategy({
-  clientID: process.env.SERVER_CLIENT_ID,
-  clientSecret: process.env.SERVER_CLIENT_SECRET,
-  callbackURL: process.env.SERVER_CLIENT_REDIRECT,
+  clientID: process.env.BOT_TEST_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/redirect",
   scope: ["identify", "guilds", "guilds.join"],
-  state: {},
-}, async (accessToken, refreshToken, profile, done, state) => {
+  state: true,
+}, async (request, response, accessToken, refreshToken, profile, done) => {
+  console.log(profile.id);
   const client = new Discord.Client();
   await client.login(process.env.BOT_TOKEN);
   const guild = await client.guilds.fetch(process.env.GUILD_ID);
-  const members = await guild.members.fetch();
-  const member = members.get(profile.id);
-  const role = guild.roles.cache.find(r => r.name === "test");
+  const roles = await guild.roles.fetch();
+  const courseRole = roles.cache.find(r => r.name === request.query.state);
+  if (!courseRole) response.redirect(process.env.SERVER_URL);
+  const member = guild.members.cache.get(profile.id);
   if (member) {
-    await member.roles.add(role);
-    member.fetch(true);
+    await member.roles.add(courseRole);
+    response.json({ courseRole, member });
   }
   else {
-    await client.users.fetch(profile.id).then((user) => {
-      // Add the user to the guild - make sure you pass the access token.
-      guild.addMember(user, { accessToken, roles: [role] });
-    });
+    client.users.fetch(profile.id)
+      .then((user) => guild.addMember(user, { accessToken, roles: [courseRole.id] }));
+    response.redirect(process.env.SERVER_URL);
   }
-}));
+
+// response.json({ courseRole, member });
+// response.json({ status: "authentication failed" });
+},
+));
