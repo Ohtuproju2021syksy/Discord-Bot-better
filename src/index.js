23033a41a12e1e25c357a0f70efb68e9d5c690cb
larikkai <kai.larikka@kolumbus.fi>
2021-07-10 01:53:11 +0300

    Update server
    - add state
    - update test to close server
    - refactor server directory
    - import server index into bot index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 require("./db/index");
 const startDiscordBot = require("./discordBot/index");
 const startServer = require("./server/server");
 const { client } = startDiscordBot;
+=======
+require("dotenv").config();
+require("./server/index");
+const Discord = require("discord.js");
+const fs = require("fs");
+>>>>>>> a9dc541 (Update server)
 
 startServer(client);
--- src/server/strategies/discordstrategy.js
+++ src/server/strategies/discordstrategy.js
@@ -12,7 +12,11 @@ passport.deserializeUser((user, done) => {
 passport.use(new DiscordStrategy({
   clientID: process.env.BOT_TEST_ID,
   clientSecret: process.env.CLIENT_SECRET,
+<<<<<<< HEAD
   callbackURL: process.env.DISCORD_REDIRECT_URL,
+=======
+  callbackURL: "http://localhost:3000/discordAuth",
+>>>>>>> a9dc541 (Update server)
   scope: ["identify", "guilds", "guilds.join"],
   store: true,
 }, async (accessToken, refreshToken, profile, done) => {
