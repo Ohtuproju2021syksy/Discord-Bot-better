224425d4152a332f395f1bbc2d3cfeb7fcf31bed
larikkai <kai.larikka@kolumbus.fi>
2021-07-10 01:59:06 +0300

    Update backend
    - change route /:course to /invite/:course
    - change default redirect / to /discordAuth
    - add default route / to send status ok
    - add default router handler to redirect /

--- src/backend/index.js
+++ src/backend/index.js
@@ -8,9 +8,12 @@ const clientSecret = process.env.CLIENT_SECRET;
 
 const app = express();
 
-app.get("/", async (request, response) => {
+app.get("/", (request, response) => {
+  response.status(200).send("OK");
+});
+
+app.get("/discordAuth", async (request, response) => {
   const { code, state } = request.query;
-  // console.log(state);
   if (code) {
     try {
       const oauthResult = await fetch("https://discord.com/api/oauth2/token", {
@@ -29,7 +32,6 @@ app.get("/", async (request, response) => {
       });
 
       const oauthData = await oauthResult.json();
-      // console.log(oauthData);
       const accessToken = oauthData.access_token;
       const userResult = await fetch("https://discord.com/api/users/@me", {
         headers: {
@@ -37,39 +39,45 @@ app.get("/", async (request, response) => {
         },
       });
       const authedUser = await userResult.json();
-      // console.log(authedUser);
-
       const { client } = require("../index");
       const guild = await client.guilds.fetch(process.env.GUILD_ID);
       const courseRole = guild.roles.cache.find(r => r.name === state);
-      if (!courseRole) response.json({ status: "invalid invite url" });
       const member = guild.members.cache.get(authedUser.id);
       if (member) {
         await member.roles.add(courseRole);
-        response.redirect(process.env.DISCORD_SERVER_INVITE);
       }
       else {
         client.users.fetch(authedUser.id)
           .then((user) => guild.addMember(user, { accessToken, roles: [courseRole.id] }));
-        response.redirect(process.env.DISCORD_SERVER_INVITE);
       }
-
-      // response.json({ courseRole, member });
+      response.redirect(process.env.DISCORD_SERVER_INVITE);
     }
     catch (error) {
-      // NOTE: An unauthorized token will not throw an error;
-      // it will return a 401 Unauthorized response in the try block above
       console.error(error);
     }
   }
-  // response.json({ status: "authentication failed" });
+  else {
+    response.status(401).send("Unauthorized client");
+  }
 });
 
-app.get("/:course", async ({ params }, response) => {
+app.get("/invite/:course", async ({ params }, response) => {
   const { course } = params;
-  const redirect_uri =
+  const { client } = require("../index");
+  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+  const courseRole = guild.roles.cache.find(r => r.name === course);
+  if (courseRole) {
+    const redirect_uri =
     `${process.env.DISCORD_REDIRECT_URL}&state=${course}`;
-  response.redirect(redirect_uri);
+    response.redirect(redirect_uri);
+  }
+  else {
+    response.status(400).send("Bad Request");
+  }
+});
+
+app.all("*", function(request, response) {
+  response.redirect("/");
 });
 
 if (process.env.NODE_ENV !== "test") app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
