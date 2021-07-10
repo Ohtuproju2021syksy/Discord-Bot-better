a55d14110236076c8ae373e97245d292bbf6aaf8
larikkai <kai.larikka@kolumbus.fi>
2021-07-10 03:43:48 +0300

    Update server
    - rename invites to invite
    - default root route handler
    - add default route error handler
    - add invite/:params to state

--- src/server/routes/invites.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const passport = require("passport");
-
-router.get("/:id", passport.authenticate("discord", {
-  session: false,
-  state: { courseName: "test" },
-}));
-
-module.exports = router;
