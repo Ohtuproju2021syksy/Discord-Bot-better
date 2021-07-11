864ff24779b30db865aca3ab94fd1560770943d5
larikkai <kai.larikka@kolumbus.fi>
2021-07-11 18:59:27 +0300

    Update jest tests
    - Move old ones to temp folder
    - Create new ones with mocked api calls

---
+++ __tests__/jest/initialize.test.js
@@ -0,0 +1,33 @@
+const { initializeApplicationContext } = require("../../src/init");
+
+jest.mock("../../src/service");
+
+const client = {
+  user: {
+    id: 1,
+  },
+  guild: {
+    channels: {
+      cache: [],
+      create: jest.fn((name) => client.guild.channels.cache.push({
+        name: name, type: "text",
+        send: jest.fn((content) => { return { content: content, pin: jest.fn() }; }),
+        lastPinTimestamp: null,
+        createInvite: jest.fn(),
+      })),
+    },
+    roles: {
+      cache: [],
+      create: jest.fn(),
+    },
+    fetchInvites: jest.fn(() => []),
+  },
+};
+
+describe("Initialize", () => {
+  test("found command channel", async () => {
+    await initializeApplicationContext(client);
+    expect(client.guild.channels.create).toHaveBeenCalledTimes(2);
+    expect(client.guild.channels.cache.length).toBe(2);
+  });
+});
