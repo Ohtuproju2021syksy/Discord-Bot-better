b1ef384063b836791dec6e5634ef4e9f267cab4a
larikkai <kai.larikka@kolumbus.fi>
2021-07-11 18:45:46 +0300

    Remove client circular dependency

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const startServer = require("./server/server");
 const { client } = startDiscordBot;
 =======
 require("dotenv").config();
-require("./server/index");
 const Discord = require("discord.js");
 const fs = require("fs");
 >>>>>>> a9dc541 (Update server)
