ec0a09c9c35d7baad53862eb34d5594a0e68a53a
larikkai <kai.larikka@kolumbus.fi>
2021-07-11 19:57:15 +0300

    Refactor project applications into your own directories

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 require("./db/index");
 const startDiscordBot = require("./discordBot/index");
 const startServer = require("./server/server");
 const { client } = startDiscordBot;
-=======
-require("dotenv").config();
-const Discord = require("discord.js");
-const fs = require("fs");
->>>>>>> a9dc541 (Update server)
 
 startServer(client);
