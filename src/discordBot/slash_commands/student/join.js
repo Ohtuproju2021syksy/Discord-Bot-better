e37442cd8463b46d4bf9aabf4dd3b73220f66f76
larikkai <kai.larikka@kolumbus.fi>
2021-07-13 23:13:00 +0300

    Uniform slashcommands functions

--- src/discordBot/slash_commands/student/join.js
+++ src/discordBot/slash_commands/student/join.js
@@ -3,21 +3,22 @@ const { sendEphemeral } = require("../utils");
 
 const execute = async (interaction, client) => {
   const roleString = interaction.data.options[0].value;
-  const member = await client.guild.members.fetch(interaction.member.user.id);
+  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+  const member = guild.members.cache.get(interaction.member.user.id);
+
   const courseRole = client.guild.roles.cache.find(r => r.name === roleString);
 
   const courseRoles = client.guild.roles.cache
     .filter(r => (r.name === `${roleString} admin` || r.name === `${roleString}`))
     .map(r => r.name);
 
-  if (!courseRoles.length) return sendEphemeral(client, interaction, `Invalid course name: ${roleString}`);
+  // if (!courseRoles.length) return sendEphemeral(client, interaction, `Invalid course name: ${roleString}`);
   if (member.roles.cache.some(r => courseRoles.includes(r.name))) return sendEphemeral(client, interaction, `You are already on a ${roleString} course.`);
 
   const teacher = member.roles.cache.find(r => r.name === "teacher");
   teacher ? client.guild.roles.cache
     .filter(r => (r.name === `${roleString} admin` || r.name === `${roleString}`))
     .map(async r => member.roles.add(r)) : await member.roles.add(courseRole);
-
   sendEphemeral(client, interaction, `You have been added to a ${roleString} course.`);
   updateGuide(client.guild);
 };
