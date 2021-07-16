84e69ecf0c0823ef4e92a58d2169c0724d21a7b4
Kai Larikka <50417871+larikkai@users.noreply.github.com>
2021-07-16 05:16:38 +0300

    Add jest test for slash command instructors

---
+++ __tests__/jest/slash_instructors.test.js
@@ -0,0 +1,60 @@
+const { execute } = require("../../src/discordBot/slash_commands/student/instructors");
+const { sendEphemeral } = require("../../src/discordBot/slash_commands/utils");
+
+jest.mock("../../src/discordBot/slash_commands/utils");
+
+const { intInsWithoutArgs,
+  intInsWithValidArgs,
+  intInsWithInvalidArgs,
+  intInsWithoutArgsInCourseChannelWithAdmins } = require("../temp/mockInteraction");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("slash insctuctors command", () => {
+  test("instructors command used outside course channels", async () => {
+    const client = intInsWithoutArgs.client;
+    await execute(intInsWithoutArgs, client);
+    expect(sendEphemeral).toHaveBeenCalledTimes(1);
+    expect(sendEphemeral).toHaveBeenCalledWith(client, intInsWithoutArgs, "Provide course name as argument or use the command in course channel.");
+  });
+
+  test("instructors command used without course admins", async () => {
+    const roleString = intInsWithInvalidArgs.data.options[0].value;
+    const client = intInsWithInvalidArgs.client;
+    await execute(intInsWithInvalidArgs, client);
+    expect(sendEphemeral).toHaveBeenCalledTimes(1);
+    expect(sendEphemeral).toHaveBeenCalledWith(client, intInsWithInvalidArgs, `No instructors for ${roleString}`);
+  });
+
+  test("instructors command used with course admins withargs", async () => {
+    const roleString = intInsWithValidArgs.data.options[0].value;
+    const client = intInsWithValidArgs.client;
+    client.guild.roles.cache.push({ name: `${roleString} admin`, members: [{ nickname: "teacher" }] });
+    await execute(intInsWithValidArgs, client);
+    expect(sendEphemeral).toHaveBeenCalledTimes(1);
+    expect(sendEphemeral).toHaveBeenCalledWith(client, intInsWithValidArgs, `Here are the instructors for ${roleString}: teacher`);
+    client.guild.roles.cache = [];
+  });
+
+  test("no course admins", async () => {
+    const roleString = intInsWithValidArgs.data.options[0].value;
+    const client = intInsWithValidArgs.client;
+    client.guild.roles.cache.push({ name: `${roleString} admin`, members: [] });
+    await execute(intInsWithValidArgs, client);
+    expect(sendEphemeral).toHaveBeenCalledTimes(1);
+    expect(sendEphemeral).toHaveBeenCalledWith(client, intInsWithValidArgs, `No instructors for ${roleString}`);
+    client.guild.roles.cache = [];
+  });
+
+  test("instructors command used with course admins in course channel without args", async () => {
+    const client = intInsWithoutArgsInCourseChannelWithAdmins.client;
+    const roleString = client.guild.channels.cache.get(intInsWithoutArgsInCourseChannelWithAdmins.channel_id).parent.name;
+    client.guild.roles.cache.push({ name: "test admin", members: [{ nickname: "teacher" }] });
+    await execute(intInsWithoutArgsInCourseChannelWithAdmins, client);
+    expect(sendEphemeral).toHaveBeenCalledTimes(1);
+    expect(sendEphemeral).toHaveBeenCalledWith(client, intInsWithoutArgsInCourseChannelWithAdmins, "Here are the instructors for test: teacher");
+    client.guild.roles.cache = [];
+  });
+});
