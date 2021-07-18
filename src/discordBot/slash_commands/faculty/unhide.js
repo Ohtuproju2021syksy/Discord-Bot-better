b4a934c55f28bbcabca3ba3f182add8faab709cc
Kai Larikka <50417871+larikkai@users.noreply.github.com>
2021-07-18 21:46:21 +0300

    Update hide command to use service helper functions and add cooldown

--- src/discordBot/slash_commands/faculty/unhide.js
+++ src/discordBot/slash_commands/faculty/unhide.js
@@ -1,25 +1,32 @@
-const { updateGuide, createPrivateCategoryName } = require("../../services/service");
+const { updateGuide, createPrivateCategoryName, findChannelWithNameAndType,
+  msToMinutesAndSeconds, handleCooldown } = require("../../services/service");
 const { sendEphemeral } = require("../utils");
+
+const used = new Map();
 
 const execute = async (interaction, client) => {
   const courseName = interaction.data.options[0].value.toLowerCase().trim();
-
   const guild = client.guild;
-
   const courseString = createPrivateCategoryName(courseName);
-  const category = guild.channels.cache.find(c => c.type === "category" && c.name === courseString);
-  if (category) {
-    await category.setName(`📚 ${courseName}`)
-      .catch(console.error);
-  }
-  else {
+  const category = findChannelWithNameAndType(courseString, "category", guild);
+  if (!category) {
     return sendEphemeral(client, interaction, `Invalid course name: ${courseName} or the course is public already.`);
   }
-
-  sendEphemeral(client, interaction, `This course ${courseName} is now public.`);
-
-  await client.emit("COURSES_CHANGED");
-  await updateGuide(client.guild);
+  const cooldown = used.get(courseName);
+  if (cooldown) {
+    const timeRemaining = Math.floor(cooldown - Date.now());
+    const time = msToMinutesAndSeconds(timeRemaining);
+    return sendEphemeral(client, interaction, `Command cooldown [mm:ss]: you need to wait ${time}.`);
+  }
+  else {
+    await category.setName(`📚 ${courseName}`);
+    sendEphemeral(client, interaction, `This course ${courseName} is now public.`);
+    const cooldownTimeMs = 1000 * 60 * 15;
+    used.set(courseName, Date.now() + cooldownTimeMs);
+    handleCooldown(used, courseName, cooldownTimeMs);
+    await client.emit("COURSES_CHANGED");
+    await updateGuide(client.guild);
+  }
 };
 
 module.exports = {
