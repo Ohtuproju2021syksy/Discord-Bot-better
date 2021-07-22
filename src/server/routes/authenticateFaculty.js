5b52a842ea4dea89430c2b5b7ce05def8da74a30
Kai Larikka <50417871+larikkai@users.noreply.github.com>
2021-07-22 22:27:44 +0300

    Update authenticate faculty route

--- src/server/routes/authenticateFaculty.js
+++ src/server/routes/authenticateFaculty.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (request, response) => {
-  console.log(request.headers.employeenumber)
+  console.log(request.headers.employeenumber);
   response.send("You have reached the path to authenticate as a faculty member. Beware!");
 });
 
