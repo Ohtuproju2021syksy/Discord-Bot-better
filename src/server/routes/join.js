28a0c68b5504fd4aa724aabe115b6bc2062b8a61
Kai Larikka <50417871+larikkai@users.noreply.github.com>
2021-07-22 23:11:41 +0300

    Uniform role in routes

--- src/server/routes/join.js
+++ src/server/routes/join.js
@@ -4,14 +4,14 @@ const passport = require("passport");
 module.exports = (client) => {
   router.get("/:id", async (req, res, next) => {
     const guild = await client.guilds.fetch(process.env.GUILD_ID);
-    const courseRole = guild.roles.cache.find(r => r.name === req.params.id);
-    if (!courseRole) {
+    const role = guild.roles.cache.find(r => r.name === req.params.id);
+    if (!role) {
       res.sendStatus(400);
       return;
     }
     passport.authenticate("discord", {
       session: false,
-      state: { courseRoleID: courseRole.id },
+      state: { roleID: role.id },
     })(req, res, next);
   });
   return router;
