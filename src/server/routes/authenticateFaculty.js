5489153660514d10447a137481a28d4b4fafe062
Kai Larikka <50417871+larikkai@users.noreply.github.com>
2021-07-23 00:13:51 +0300

    Change facultyAuth to give teacher role

--- src/server/routes/authenticateFaculty.js
+++ src/server/routes/authenticateFaculty.js
@@ -5,14 +5,14 @@ module.exports = (client) => {
   router.get("/", async (req, res, next) => {
     if (!req.headers.employeenumber) res.sendStatus(401);
     const guild = await client.guilds.fetch(process.env.GUILD_ID);
-    const facultyRole = guild.roles.cache.find(r => r.name === "faculty");
-    if (!facultyRole) {
+    const teacherRole = guild.roles.cache.find(r => r.name === "teacher");
+    if (!teacherRole) {
       res.sendStatus(400);
       return;
     }
     passport.authenticate("discord", {
       session: false,
-      state: { roleID: facultyRole.id },
+      state: { roleID: teacherRole.id },
     })(req, res, next);
   });
   return router;
