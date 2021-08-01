c6a29af8e5487a8edc16c6a63bfe1377050e3b87
Kai Larikka <50417871+larikkai@users.noreply.github.com>
2021-08-01 22:41:06 +0300

    Add more message event tests

--- __tests__/jest/message.test.js
+++ __tests__/jest/message.test.js
@@ -1,18 +1,54 @@
 const { execute } = require("../../src/discordBot/events/message");
 
-const { messageInGuideChannel } = require("../mocks/mockMessages");
+const { messageInGuideChannel, messageInCommandsChannel, student } = require("../mocks/mockMessages");
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 describe("prefix commands", () => {
-  test("prefix commands cannot be used in guide channel", async () => {
+  test("commands cannot be used in guide channel", async () => {
     messageInGuideChannel.content = "!join test";
     const client = messageInGuideChannel.client;
     await execute(messageInGuideChannel, client);
     expect(messageInGuideChannel.channel.send).toHaveBeenCalledTimes(0);
     expect(messageInGuideChannel.react).toHaveBeenCalledTimes(0);
     expect(messageInGuideChannel.reply).toHaveBeenCalledTimes(0);
+  });
+
+  test("invalid command in commands channel does nothing", async () => {
+    messageInCommandsChannel.content = "!join test";
+    const client = messageInCommandsChannel.client;
+    await execute(messageInCommandsChannel, client);
+    expect(messageInCommandsChannel.channel.send).toHaveBeenCalledTimes(0);
+    expect(messageInCommandsChannel.react).toHaveBeenCalledTimes(0);
+    expect(messageInCommandsChannel.reply).toHaveBeenCalledTimes(0);
+  });
+
+  test("command misuse sends corrent info ", async () => {
+    messageInCommandsChannel.content = "!deletecommand";
+    const client = messageInCommandsChannel.client;
+    await execute(messageInCommandsChannel, client);
+    expect(messageInCommandsChannel.channel.send).toHaveBeenCalledTimes(1);
+    expect(messageInCommandsChannel.channel.send).toHaveBeenCalledWith(`You didn't provide any arguments, ${messageInCommandsChannel.author}!`);
+  });
+
+  test("valid commands can be used in commands channel", async () => {
+    messageInCommandsChannel.content = "!sort";
+    const client = messageInCommandsChannel.client;
+    await execute(messageInCommandsChannel, client);
+    expect(messageInCommandsChannel.react).toHaveBeenCalledTimes(1);
+    expect(messageInCommandsChannel.react).toHaveBeenCalledWith("✅");
+  });
+
+  test("if no command role do nothing", async () => {
+    messageInCommandsChannel.content = "!sort";
+    const client = messageInCommandsChannel.client;
+    messageInCommandsChannel.author = student;
+    messageInCommandsChannel.member = student;
+    await execute(messageInCommandsChannel, client);
+    expect(messageInCommandsChannel.channel.send).toHaveBeenCalledTimes(0);
+    expect(messageInCommandsChannel.react).toHaveBeenCalledTimes(0);
+    expect(messageInCommandsChannel.reply).toHaveBeenCalledTimes(0);
   });
 });
