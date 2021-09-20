5a63ebc0975bd9fb99ac7ccea0250ffc2e9a7efa
Eemeli Saarinen <saarineneemelia@gmail.com>
2021-09-20 15:59:09 +0300

    Removed a debug message

--- src/discordBot/events/messageCreate.js
+++ src/discordBot/events/messageCreate.js
@@ -6,20 +6,22 @@ const execute = async (message, client, Course) => {
   const commandName = args.shift().toLowerCase();
   args = args.map(arg => arg.toLowerCase().trim());
 
+  // Copypasted slash command
   if (client.slashCommands.has(commandName)) {
     const command = client.slashCommands.get(commandName);
     if (commandName == "join" || commandName == "leave") {
       const roleString = args.shift().toLowerCase().trim();
       if (!roleString) return;
+      // Command execution handles permissions and whether the course is valid
       message.roleString = roleString;
       command.execute(message, client, Course);
       return;
     }
   }
 
+  // Prefix command
   if (!client.commands.has(commandName)) return;
   const command = client.commands.get(commandName);
-  console.log(command.role);
   if (command.role && !message.member.roles.cache.find(r => r.name === command.role)) return;
   if (command.args && !args.length) {
     return message.channel.send({ content: `You didn't provide any arguments, ${message.author}!`, reply: { messageReference: message.id } });
