3bb01fce26ebd2ad637a75351fa2047462f941ed
Mikko <m.a.pelkonen2@gmail.com>
2021-09-26 12:50:20 +0300

    Removed unnecessary code

--- src/promMetrics/promRegistry.js
+++ src/promMetrics/promRegistry.js
@@ -1,12 +1,9 @@
 const promClient = require("prom-client");
-const messageCounter = require("./messageCounter");
 const joinedUsersCounter = require("./joinedUsersCounter");
 
 const register = new promClient.Registry();
 
 const initPromRegistry = () => {
-  // promClient.collectDefaultMetrics({ register });
-  register.registerMetric(messageCounter);
   register.registerMetric(joinedUsersCounter);
 };
 
