c3b9c0a0517346713af0fbff364802bb7bce5b8e
Timo Reijonen <timo.reijonen@helsinki.fi>
2021-10-17 19:47:23 +0300

    New command update_categorynames

---
+++ src/discordBot/commands/admin/update_categorynames.js
@@ -0,0 +1,20 @@
+const { trimCourseName } = require("../../services/service");
+
+const execute = async (message) => {
+  if (message.member.permissions.has("ADMINISTRATOR")) {
+    const channels = message.guild.channels.cache.filter(c => c.type === "GUILD_CATEGORY" && c.name.includes("🔒"));
+    channels.forEach(async channel => {
+      await channel.setName(`👻 ${trimCourseName(channel)}`);
+    });
+  }
+};
+
+module.exports = {
+  prefix: true,
+  name: "update_categorynames",
+  description: "Updates category names to the new format",
+  role: "admin",
+  usage: "!update_categorynames",
+  args: false,
+  execute,
+};
