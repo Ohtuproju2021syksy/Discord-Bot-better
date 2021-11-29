f3b83ba3210adc0e2c21da5fc5e0808f5a486201
Okko Kuisma <okkokuisma@gmail.com>
2021-11-29 17:25:21 +0200

    channel discordIds and course categoryIds are saved to database

--- src/db/hookInit.js
+++ src/db/hookInit.js
@@ -21,7 +21,7 @@ const { findCourseFromDbById } = require("./services/courseService");
 const { findUserByDbId } = require("./services/userService");
 const { courseAdminRole, facultyRole } = require("../../config.json");
 const { Op } = require("sequelize");
-const { editChannelNames } = require("../db/services/channelService");
+const { editChannelNames, createDefaultChannelsToDatabase } = require("../db/services/channelService");
 
 const initHooks = (guild, models) => {
   initChannelHooks(guild, models);
@@ -45,7 +45,8 @@ const initChannelHooks = (guild, models) => {
       const category = findCategoryWithCourseName(course.name, guild);
       const channelObject = getChannelObject(course.name, channelName, category);
 
-      await findOrCreateChannel(channelObject, guild);
+      const createdChannel = await findOrCreateChannel(channelObject, guild);
+      await channel.update({ discordId: createdChannel.id });
     }
   });
 
@@ -58,9 +59,11 @@ const initChannelHooks = (guild, models) => {
     const category = await findOrCreateChannel(categoryObject, guild);
 
     const channelObjects = await getDefaultChannelObjects(guild, course.name, student, admin, category);
-    await Promise.all(channelObjects.map(
-      async channelObject => await findOrCreateChannel(channelObject, guild),
-    ));
+    await Promise.all(channelObjects.map(async channelObject => {
+      const createdChannel = await findOrCreateChannel(channelObject, guild);
+      const channelInstance = channel.find(c => c.name === createdChannel.name);
+      await channelInstance.update({ discordId: createdChannel.id });
+    }));
 
     await setCoursePositionABC(guild, categoryObject.name, courseModel);
     await createInvitation(guild, course.name);
@@ -69,7 +72,7 @@ const initChannelHooks = (guild, models) => {
   });
 
   channelModel.addHook("afterUpdate", async (channel) => {
-    if (channel._changed.has("name")) {
+    if (channel._changed.has("name") && channel._previousDataValues.name) {
       const channelObject = guild.channels.cache
         .find(c => c.name === channel._previousDataValues.name);
       await channelObject.setName(channel.name);
@@ -103,61 +106,66 @@ const initCourseHooks = (guild, models) => {
   });
 
   models.Course.addHook("afterCreate", async (course) => {
-    const courseModel = models.Course;
     const student = await findOrCreateRoleWithName(course.name, guild);
     const admin = await findOrCreateRoleWithName(`${course.name} ${courseAdminRole}`, guild);
     const categoryObject = getCategoryObject(course.name, getCategoryChannelPermissionOverwrites(guild, admin, student));
     const category = await findOrCreateChannel(categoryObject, guild);
+    await course.update({ categoryId: category.id });
 
     const channelObjects = await getDefaultChannelObjects(guild, course.name, student, admin, category);
-    await Promise.all(channelObjects.map(
-      async channelObject => await findOrCreateChannel(channelObject, guild),
-    ));
+    const defaultChannelObjects = channelObjects.map(channelObject => {
+      const voiceChannel = channelObject.options.type === "GUILD_VOICE";
+      return {
+        courseId: course.id,
+        name: channelObject.name,
+        defaultChannel: true,
+        voiceChannel: voiceChannel,
+      };
+    });
 
-    await setCoursePositionABC(guild, categoryObject.name, courseModel);
-    await createInvitation(guild, course.name);
-    await guild.client.emit("COURSES_CHANGED", courseModel);
-    await updateGuide(guild, models);
+    await createDefaultChannelsToDatabase(defaultChannelObjects, models.Channel);
   });
 
   models.Course.addHook("afterUpdate", async (course) => {
-    const changedValue = course._changed;
-    const courseName = course.name;
-    const previousCourseName = course._previousDataValues.name;
-    let category = findCategoryWithCourseName(courseName, guild);
-    const hidden = course.private;
-    const locked = course.locked;
+    if (!course._options.isNewRecord) {
+      const changedValue = course._changed;
+      const courseName = course.name;
+      const previousCourseName = course._previousDataValues.name;
+      let category = findCategoryWithCourseName(courseName, guild);
+      const hidden = course.private;
+      const locked = course.locked;
 
-    if (changedValue.has("locked")) {
-      if (locked) {
-        await lockTelegramCourse(models.Course, courseName);
-        await setEmojisLock(category, hidden, courseName, models);
-        category.permissionOverwrites.create(guild.roles.cache.find(r => r.name.toLowerCase().includes(courseName.toLowerCase())), { VIEW_CHANNEL: true, SEND_MESSAGES: false });
-        category.permissionOverwrites.create(guild.roles.cache.find(r => r.name === "faculty"), { SEND_MESSAGES: true });
-        category.permissionOverwrites.create(guild.roles.cache.find(r => r.name === "admin"), { SEND_MESSAGES: true });
+      if (changedValue.has("locked")) {
+        if (locked) {
+          await lockTelegramCourse(models.Course, courseName);
+          await setEmojisLock(category, hidden, courseName, models);
+          category.permissionOverwrites.create(guild.roles.cache.find(r => r.name.toLowerCase().includes(courseName.toLowerCase())), { VIEW_CHANNEL: true, SEND_MESSAGES: false });
+          category.permissionOverwrites.create(guild.roles.cache.find(r => r.name === "faculty"), { SEND_MESSAGES: true });
+          category.permissionOverwrites.create(guild.roles.cache.find(r => r.name === "admin"), { SEND_MESSAGES: true });
+        }
+        else {
+          await unlockTelegramCourse(models.Course, courseName);
+          await setEmojisUnlock(category, hidden, courseName, models);
+          category.permissionOverwrites.create(guild.roles.cache.find(r => r.name.toLowerCase().includes(courseName.toLowerCase())), { VIEW_CHANNEL: true, SEND_MESSAGES: true });
+        }
       }
-      else {
-        await unlockTelegramCourse(models.Course, courseName);
-        await setEmojisUnlock(category, hidden, courseName, models);
-        category.permissionOverwrites.create(guild.roles.cache.find(r => r.name.toLowerCase().includes(courseName.toLowerCase())), { VIEW_CHANNEL: true, SEND_MESSAGES: true });
+      else if (changedValue.has("private")) {
+        hidden ?
+          await setEmojisHide(category, locked, courseName)
+          : await setEmojisUnhide(category, locked, courseName);
       }
+      else if (changedValue.has("name")) {
+        category = findCategoryWithCourseName(previousCourseName, guild);
+        const channelAnnouncement = guild.channels.cache.find(c => c.name === `${previousCourseName}_announcement`);
+        const categoryEmojis = category.name.replace(previousCourseName, "").trim();
+        await category.setName(`${categoryEmojis} ${courseName}`);
+        await changeCourseRoles(previousCourseName, courseName, guild);
+        await setCoursePositionABC(guild, `${categoryEmojis} ${courseName}`, models.Course);
+        await editChannelNames(course.id, previousCourseName, courseName, models.Channel);
+        await updateAnnouncementChannelMessage(guild, channelAnnouncement);
+      }
+      await updateGuide(guild, models);
     }
-    else if (changedValue.has("private")) {
-      hidden ?
-        await setEmojisHide(category, locked, courseName)
-        : await setEmojisUnhide(category, locked, courseName);
-    }
-    else if (changedValue.has("name")) {
-      category = findCategoryWithCourseName(previousCourseName, guild);
-      const channelAnnouncement = guild.channels.cache.find(c => c.name === `${previousCourseName}_announcement`);
-      const categoryEmojis = category.name.replace(previousCourseName, "").trim();
-      await category.setName(`${categoryEmojis} ${courseName}`);
-      await changeCourseRoles(previousCourseName, courseName, guild);
-      await setCoursePositionABC(guild, `${categoryEmojis} ${courseName}`, models.Course);
-      await editChannelNames(course.id, previousCourseName, courseName, models.Channel);
-      await updateAnnouncementChannelMessage(guild, channelAnnouncement);
-    }
-    await updateGuide(guild, models);
   });
 };
 
