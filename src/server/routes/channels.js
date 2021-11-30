b40ae51080d130d6a0c4a4ce853347aba77951e4
Teemu Närhi <t.a.narhi@gmail.com>
2021-12-01 01:52:22 +0200

    POST for adding text channel to course

--- src/server/routes/channels.js
+++ src/server/routes/channels.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const models = require("../../db/dbInit");
-const { findChannelsFromDb, findChannelsByCourse, findChannelFromDbById } = require("../../db/services/channelService");
+const { findChannelsFromDb, findChannelsByCourse, findChannelFromDbById, createChannelToDatabase } = require("../../db/services/channelService");
+const { findCourseFromDbById } = require("../../db/services/courseService");
 const { logError } = require("../../discordBot/services/logger");
 
 router.get("/", async (req, res) => {
@@ -27,6 +28,20 @@ router.get("/ofCourse/:id", async (req, res) => {
   try {
     const channels = await findChannelsByCourse(req.params.id, models.Channel);
     channels ? res.json(channels).status(200) : res.status(404).end();
+  }
+  catch (error) {
+    logError(error);
+  }
+});
+
+router.post("/ofCourse/:id", async (req, res) => {
+  try {
+    const course = await findCourseFromDbById(req.params.id, models.Course);
+    if (!course) {
+      res.status(404).end();
+    }
+    const newChannel = await createChannelToDatabase({ courseId: course.id, name: req.body.name }, models.Channel);
+    res.json(newChannel).status(200);
   }
   catch (error) {
     logError(error);
