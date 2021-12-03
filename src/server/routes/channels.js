6fdb22114d3319070c81305b22af23462495581e
Teemu Närhi <t.a.narhi@gmail.com>
2021-12-03 02:49:08 +0200

    PUT and DELETE for channels

--- src/server/routes/channels.js
+++ src/server/routes/channels.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const models = require("../../db/dbInit");
-const { findChannelsFromDb, findChannelsByCourse, findChannelFromDbById, createChannelToDatabase } = require("../../db/services/channelService");
+const { findChannelsFromDb, findChannelsByCourse, findChannelFromDbById, createChannelToDatabase, removeChannelFromDb } = require("../../db/services/channelService");
 const { findCourseFromDbById } = require("../../db/services/courseService");
 const { logError } = require("../../discordBot/services/logger");
 
@@ -34,14 +34,40 @@ router.get("/ofCourse/:id", async (req, res) => {
   }
 });
 
-router.post("/ofCourse/:id", async (req, res) => {
+router.post("/:id", async (req, res) => {
   try {
     const course = await findCourseFromDbById(req.params.id, models.Course);
-    if (!course) {
-      res.status(404).end();
-    }
+
     const newChannel = await createChannelToDatabase({ courseId: course.id, name: req.body.name }, models.Channel);
-    res.json(newChannel).status(200);
+    res.json(newChannel).status(201);
+  }
+  catch (error) {
+    logError(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const channel = await findChannelFromDbById(req.params.id, models.Channel);
+    channel.name = req.body.name;
+    channel.topic = req.body.topic;
+    channel.bridged = req.body.bridged;
+    await channel.save();
+    res.json(channel).status(200);
+  }
+  catch (error) {
+    logError(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const channel = await findChannelFromDbById(req.params.id, models.Channel);
+    if (channel.defaultChannel) {
+      res.status(405).end();
+    }
+    await removeChannelFromDb(req.body.name, models.Channel);
+    res.status(204).end();
   }
   catch (error) {
     logError(error);
