1fb5aae9f12d7a7ba2e7b67ea35b8f658d163293
Timo Reijonen <timo.reijonen@helsinki.fi>
2021-12-20 20:12:19 +0200

    Fix /instructors bug

--- src/discordBot/commands/student/instructors.js
+++ src/discordBot/commands/student/instructors.js
@@ -2,22 +2,24 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { getCourseNameFromCategory, listCourseInstructors } = require("../../services/service");
 const { editEphemeral, editErrorEphemeral, sendEphemeral } = require("../../services/message");
 const { courseAdminRole, facultyRole } = require("../../../../config.json");
+const { findCourseFromDb } = require("../../../db/services/courseService");
 
-const execute = async (interaction, client) => {
+const execute = async (interaction, client, models) => {
   await sendEphemeral(interaction, "Fetching instructors...");
   const guild = client.guild;
 
   const category = guild.channels.cache.get(interaction.channelId).parent;
-  if (!category) {
+  const courseName = category?.name ? getCourseNameFromCategory(category.name) : null;
+  const course = await findCourseFromDb(courseName, models.Course);
+  if (!category || !course) {
     return await editErrorEphemeral(interaction, "Use the command in a course channel.");
   }
-  const roleString = getCourseNameFromCategory(category.name);
 
-  const adminsString = await listCourseInstructors(guild, roleString, courseAdminRole, facultyRole);
+  const adminsString = await listCourseInstructors(guild, courseName, courseAdminRole, facultyRole);
 
-  if (adminsString === "") return await editErrorEphemeral(interaction, `No instructors for ${roleString}`);
+  if (adminsString === "") return await editErrorEphemeral(interaction, `No instructors for ${courseName}`);
 
-  await editEphemeral(interaction, `Here are the instructors for ${roleString}: ${adminsString}`);
+  await editEphemeral(interaction, `Here are the instructors for ${courseName}: ${adminsString}`);
 };
 
 module.exports = {
